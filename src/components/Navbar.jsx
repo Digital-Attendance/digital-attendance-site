4da156cd743b342c069bc4ac2e4f59d94985463d
Rajivsah240 <rajivsah240@gmail.com>
2025-04-04 19:25:22 +0530

    reponsive improvement

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -8,18 +8,18 @@ const Navbar = () => {
 
   return (
     <nav className="navbar">
-      {/* Logo and App Name (Always Centered in Small Screens) */}
+      
       <div className="logo-container">
         <img src={logo} alt="Logo" className="logo" />
         <h1>Digital Attendance</h1>
       </div>
 
-      {/* Menu Button (Visible in Small Screens) */}
+      
       <div className="menu-icon" onClick={() => setMenuOpen(!menuOpen)}>
         ☰
       </div>
 
-      {/* Navigation Links */}
+      
       <div className={`nav-links ${menuOpen ? "open" : ""}`}>
         <Link to="/" onClick={() => setMenuOpen(false)}>Home</Link>
         <Link to="/tutorial" onClick={() => setMenuOpen(false)}>Tutorial</Link>
