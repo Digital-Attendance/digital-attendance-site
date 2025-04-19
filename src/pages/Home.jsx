eb8ba7c7f40ef12c515633e66ea0825357dd7097
Rajivsah240 <rajivsah240@gmail.com>
2025-04-19 09:46:37 +0530

    added HTTPS

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -24,7 +24,7 @@ const Home = () => {
 
   const handleDownload = async () => {
     try {
-      const res = await fetch("proper-boar-digital-attendance-99206d79.koyeb.app/download", {
+      const res = await fetch("https://proper-boar-digital-attendance-99206d79.koyeb.app/download", {
         method: "GET",
         headers: {
           "Content-Type": "application/json",
